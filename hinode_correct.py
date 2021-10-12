d997100fab3f0a4995a42502a84e7bac4f420032
imilic <ivanzmilic@gmail.com>
2021-10-11 18:06:48 -0600

    Added SPBSHFT as an input argument

--- hinode_correct.py
+++ hinode_correct.py
@@ -4,15 +4,26 @@ import sys
 
 cube_name = sys.argv[1]
 output_cube_name = sys.argv[2]
+SPBSHFT = int(sys.argv[3])
 
 stokes = fits.open(cube_name)[0].data
 stokes = stokes.astype('double')
 
+
 stokesI = np.copy(stokes[:,:,0,:])
-stokesI *= 2
+if (SPBSHFT > 0):
+	stokesI *= 2
 negative = np.where(stokesI < 0.0)
 stokesI[negative] += 65536.
 stokes[:,:,0,:] = stokesI
+
+if (SPBSHFT > 1):
+	stokes[:,:,3,:] *= 2
+
+if (SPBSHFT > 2):
+	stokes[:,:,1,:] *= 2
+	stokes[:,:,2,:] *= 2
+
 
 
 xxx = fits.PrimaryHDU(stokes)
